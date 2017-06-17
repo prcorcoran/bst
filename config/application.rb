554a367e19b5f0bd7772a7de5ef4a0380ee2ddd1
prcorcoran <prcorcoran99@gmail.com>
2017-06-17 14:43:16 -0400

    add tls certificate

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,10 @@ module Bst
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    if Rails.env.production?
+      config.force_ssl = true
+    else
+      config.force_ssl = true
+    end
   end
 end
